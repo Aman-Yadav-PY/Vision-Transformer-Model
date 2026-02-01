2392d9c14d5d9ff88d8fd32d9f5d14026a624a4d
Name <assassins2032004@gmail.com>
2026-02-01 20:12:33 +0530

    small update in pretrainedmodel class

--- modelpipe/pretrainedbasemodel.py
+++ modelpipe/pretrainedbasemodel.py
@@ -6,15 +6,17 @@ class PretrainedBaseModel(nn.Module):
     def __init__(self, pretrained_model:nn.Module=None, freeze_model=True):
         super(PretrainedBaseModel, self).__init__()
         self.pt_model = pretrained_model or efficientnet_v2_s(weights=EfficientNet_V2_S_Weights.IMAGENET1K_V1)
-        self.model = self.pt_model.features
-
+        
         if freeze_model:
             self.freeze();
 
+        self.model = self.pt_model.features
+
+        
     def freeze(self):
-        for params in self.model.parameters():
+        for params in self.pt_model.parameters():
             params.requires_grad = False
-        self.model.eval() #to prevent batchnorm running -stats(mean & std) update (?)
+        self.pt_model.eval() #to prevent batchnorm running -stats(mean & std) update (?)
 
     def forward(self, x):
         return self.model(x)
